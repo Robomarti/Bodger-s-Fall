544358a7533a65ec78f8a7a64403e2586a055d77
Robomarti <robomarti@outlook.com>
2024-09-26 16:10:46 +0300

    made cloud spawner

--- Assets/Scripts/CloudSpawner.cs
+++ Assets/Scripts/CloudSpawner.cs
@@ -1,32 +1,31 @@
-using System.Collections;
 using UnityEngine;
 
-// made with https://www.youtube.com/watch?v=vClEQ1GqMPw
-public class ObstacleSpawner : MonoBehaviour {
+public class CloudSpawner : MonoBehaviour
+{
     [SerializeField] private float obstacleSpawnInterval;
     [SerializeField] private Spawnables[] obstaclePrefabs;
-    [SerializeField] private SpawnLocations[] spawnLocations;
+    [SerializeField] private Transform[] spawnLocations;
 
     private float timeUntilObstacleSpawn;
     private GameObject obstacleToSpawn;
 
     private int spawnChances;
     private int spawnChanceCounter;
-    
-    private SpawnLocations nextSpawnLocation;
+
+    private Transform nextSpawnLocation;
 
     private void Start() {
         timeUntilObstacleSpawn = obstacleSpawnInterval;
         CountSpawnChances();
 
         SetObstacleToSpawn();
-        Instantiate(obstacleToSpawn, spawnLocations[0].spawnPoint.position, Quaternion.identity);
+        Instantiate(obstacleToSpawn, spawnLocations[0].position, Quaternion.identity);
         
         SetObstacleToSpawn();
-        Instantiate(obstacleToSpawn, spawnLocations[1].spawnPoint.position, Quaternion.identity);
+        Instantiate(obstacleToSpawn, spawnLocations[1].position, Quaternion.identity);
         
         SetObstacleToSpawn();
-        Instantiate(obstacleToSpawn, spawnLocations[2].spawnPoint.position, Quaternion.identity);
+        Instantiate(obstacleToSpawn, spawnLocations[2].position, Quaternion.identity);
     }
     
     private void Update() {
@@ -37,18 +36,11 @@ public class ObstacleSpawner : MonoBehaviour {
         timeUntilObstacleSpawn -= Time.deltaTime;
 
         if (timeUntilObstacleSpawn <= 0) {
-            StartCoroutine(SpawnObstacle());;
+            SetObstacleToSpawn();
+            nextSpawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
+            Instantiate(obstacleToSpawn, nextSpawnLocation.position, Quaternion.identity);
             timeUntilObstacleSpawn = obstacleSpawnInterval;
         }
-    }
-
-    private IEnumerator SpawnObstacle() {
-        SetObstacleToSpawn();
-        nextSpawnLocation = spawnLocations[Random.Range(0, spawnLocations.Length)];
-        nextSpawnLocation.spawnLocationWarningSign.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
-        nextSpawnLocation.spawnLocationWarningSign.SetActive(false);
-        Instantiate(obstacleToSpawn, nextSpawnLocation.spawnPoint.position, Quaternion.identity);
     }
 
     private void SetObstacleToSpawn() {
@@ -68,16 +60,5 @@ public class ObstacleSpawner : MonoBehaviour {
             spawnChances += spawnable.spawnChance;
         }
     }
-    
-    [System.Serializable]
-    public struct SpawnLocations {
-        public Transform spawnPoint;
-        public GameObject spawnLocationWarningSign;
-    }
-}
-
-[System.Serializable]
-public struct Spawnables {
-    public GameObject spawnablePrefab;
-    public int spawnChance;
+ 
 }
